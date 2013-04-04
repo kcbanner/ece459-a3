8f3adf5c21f484e38a0e8b7ec7fdbb755449107a
Stephan <stephan.vandenheuvel@gmail.com>
2013-04-04 18:03:58 -0400

    c portion of bins computation.

--- assignment-04/src/nbody-ocl-ff.c
+++ assignment-04/src/nbody-ocl-ff.c
@@ -15,10 +15,12 @@
 /// runtime on my 2011 computer: 1m; in 2013, 27s.
 // on my 2011 laptop, 1m34s
 #define POINTS 500 * 64
-#define SPACE 1000.0f;
+#define SPACE 1000.0f
+#define BINS 1000
 
 #define POSITION_BUFFER 0
 #define ACCELLERATION_BUFFER 1
+#define BUCKETS 1
 
 
 cl_float4 * initializePositions() {
@@ -39,34 +41,16 @@ cl_float4 * initializePositions() {
 }
 
 
-std::string* loadKernelSource() {
-  std::ifstream file("src/bruteforce-kernel.cl"); 
+std::string* loadKernelSource(std::string source) {
+  std::ifstream file(source.c_str()); 
   std::string* contents = new std::string((std::istreambuf_iterator<char>(file)),
                                           (std::istreambuf_iterator<char>()));
 
   return contents;
 }
 
-void runKernel(std::string sourceCode, cl_float4* x)
+void runKernel(cl::Context context, cl::CommandQueue queue, std::vector<cl::Device>& devices, std::string sourceCode, cl_float4* x)
 {
-    
-        // Get available platforms
-        std::vector<cl::Platform> platforms;
-        cl::Platform::get(&platforms);
-
-        // Select the default platform and create a context using this platform and the GPU
-        cl_context_properties cps[3] = { 
-            CL_CONTEXT_PLATFORM, 
-            (cl_context_properties)(platforms[0])(), 
-            0 
-        };
-        cl::Context context(CL_DEVICE_TYPE_GPU, cps);
-
-        // Get a list of devices on this platform
-        std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
-
-        // Create a command queue and use the first device
-        cl::CommandQueue queue = cl::CommandQueue(context, devices[0]);
 
         cl::Program::Sources source(1, std::make_pair(sourceCode.c_str(), sourceCode.length()+1));
 
@@ -126,13 +110,93 @@ void runKernel(std::string sourceCode, cl_float4* x)
     
 }
 
+void computeBins(cl::Context context, cl::CommandQueue queue, std::vector<cl::Device>& devices, std::string sourceCode, cl_float4* x)
+{
+
+        cl::Program::Sources source(1, std::make_pair(sourceCode.c_str(), sourceCode.length()+1));
+
+        // Make program of the source code in the context
+        cl::Program program = cl::Program(context, source);
+
+        // Build program for these specific devices
+    try{
+        program.build(devices);
+
+        // Make kernel
+        cl::Kernel kernel(program, "bin");
+
+        // Create memory buffers
+
+        cl::Buffer position_buffer = cl::Buffer(context, CL_MEM_READ_ONLY, POINTS * sizeof(cl_float4));
+        cl::Buffer bins_buffer = cl::Buffer(context, CL_MEM_WRITE_ONLY, BINS * sizeof(cl_float4));
+
+        // Copy lists the memory buffers
+        queue.enqueueWriteBuffer(position_buffer, CL_TRUE, 0, POINTS * sizeof(cl_float4), x);
+        
+        // Set arguments to kernel
+        //int buffer_size = POINTS;
+        //kernel.setArg(BUFFER_SIZE, buffer_size);
+        kernel.setArg(POSITION_BUFFER, position_buffer);
+        kernel.setArg(BUCKETS, bins_buffer);
+
+        // Run the kernel on specific ND range
+        cl::NDRange global(POINTS);
+        cl::NDRange local(1);
+        queue.enqueueNDRangeKernel(kernel, cl::NullRange, global, cl::NullRange);
+
+        // Read buffer C into a local list
+        cl_float4* bins = new cl_float4[BINS];
+        queue.enqueueReadBuffer(bins_buffer, CL_TRUE, 0, POINTS * sizeof(cl_float4), bins);
+
+        for(int i = 0; i < POINTS; i ++) {
+            printf("(%2.2f,%2.2f,%2.2f,%2.2f) (%2.3f,%2.3f,%2.3f)\n", 
+               x[i].x, x[i].y, x[i].z, x[i].w,
+               bins[i].x, bins[i].y, bins[i].z);
+        }
+        delete[] bins;
+
+    } catch(cl::Error error) {
+        std::cout << error.what() << "(" << error.err() << ")" << std::endl;
+
+        std::string build_log;
+        build_log = program.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(devices[0]);
+        std::cout << "Build status: " << build_log << std::endl;
+
+        build_log = program.getBuildInfo<CL_PROGRAM_BUILD_OPTIONS>(devices[0]);
+        std::cout << "Build options: " << build_log << std::endl;
+
+        build_log = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(devices[0]);
+        std::cout << "Build log: " << build_log << std::endl;
+    }
+    
+}
+
 
 int main(int argc, char ** argv)
 {
     cl_float4 * x = initializePositions();
 
-    std::string* source = loadKernelSource();
-    runKernel(*source, x);
+    std::string* bin_source = loadKernelSource("src/bin-kernel.cl");
+    // Get available platforms
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+
+    // Select the default platform and create a context using this platform and the GPU
+    cl_context_properties cps[3] = { 
+        CL_CONTEXT_PLATFORM, 
+        (cl_context_properties)(platforms[0])(), 
+        0 
+    };
+    cl::Context context(CL_DEVICE_TYPE_GPU, cps);
+
+    // Get a list of devices on this platform
+    std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
+
+    // Create a command queue and use the first device
+    cl::CommandQueue queue = cl::CommandQueue(context, devices[0]);
+
+    //runKernel(context, queue, devices, *source, x);
+    computeBins(context, queue, devices, *bin_source, x);
 
     free(x);
     return 0;
